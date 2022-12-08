e430a4f50ce76205e3a0c09f7023dcd38a639854
joyjsmun <joy.js.mun@gmail.com>
2022-12-08 21:32:27 +0900

    ADD: final commit

--- hardhat/contracts/RandomWinnerGame.sol
+++ hardhat/contracts/RandomWinnerGame.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumeBase.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 
-contract RandomWinnerGame is VRFConsumeBase, Ownable {
+contract RandomWinnerGame is VRFConsumerBase, Ownable {
     // ChainLink Variables
     // The amount of LINK to send with the request
     uint256 public fee;
@@ -13,7 +13,7 @@ contract RandomWinnerGame is VRFConsumeBase, Ownable {
     bytes32 public keyHash;
 
     // Address of the players
-    addres[] public players;
+    address[] public players;
     //Max number of players in one game
     uint8 maxPlayers;
     //Variable to indicate if the game has started or not
@@ -26,9 +26,64 @@ contract RandomWinnerGame is VRFConsumeBase, Ownable {
     // emitted when the game starts
     event GameStarted(uint256 gameId, uint8 maxPlayers, uint256 entryFee);
     // emitted when someone joins a game
-    event PlayerJoined(uint256 gameId, addres player);
+    event PlayerJoined(uint256 gameId, address player);
     // emitted when the game ends
-    event GameEnded(uint256 gameId, addres winner, bytes32 requestId);
+    event GameEnded(uint256 gameId, address winner, bytes32 requestId);
 
-    
+
+    constructor(address vrfCoordinator, address linkToken, bytes32 vrfKeyHash, uint256 vrfFee)
+    VRFConsumerBase(vrfCoordinator,linkToken) {
+        keyHash = vrfKeyHash;
+        fee = vrfFee;
+        gameStarted = false;
+    }
+
+
+    function startGame(uint8 _maxPlayers, uint256 _entryFee) public onlyOwner {
+        require(!gameStarted, "game is currently running");
+        delete players;
+        maxPlayers = _maxPlayers;
+        gameStarted= true;
+        entryFee = _entryFee;
+        gameId += 1;
+        emit GameStarted(gameId, maxPlayers, entryFee);
+    }
+
+    function joinGame() public payable{
+        require(gameStarted,"Game has not been started yet");
+        require(msg.value == entryFee, "Value sent is not the equal to entryFee"); 
+        // Check if there is still some space left in the game to add another player
+        require(players.length < maxPlayers, "Game is full");
+        // add the sender to the players list
+        players.push(msg.sender);
+        emit PlayerJoined(gameId, msg.sender);
+        if(players.length == maxPlayers){
+            getRandomWinner();
+        }
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal virtual override  {
+        // We want out winnerIndex to be in the length from 0 to players.length-1
+        // For this we mod it with the player.length value
+        uint256 winnerIndex = randomness % players.length;
+        // get the address of the winner from the players array
+        address winner = players[winnerIndex];
+        // send the ether in the contract to the winner
+        (bool sent,) = winner.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+        // Emit that the game has ended
+        emit GameEnded(gameId, winner,requestId);
+        // set the gameStarted variable to false
+        gameStarted = false;
+    }
+
+    function getRandomWinner() private returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not Enough LINK");
+        return requestRandomness(keyHash,fee);
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
+
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,31 +1,44 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
+
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const randomWinnerGame = await ethers.getContractFactory("RandomWinnerGame");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const deployedRandomWinnerGameContract = await randomWinnerGame.deploy(
+    VRF_COORDINATOR,
+    LINK_TOKEN,
+    KEY_HASH,
+    FEE
   );
+
+  await deployedRandomWinnerGameContract.deployed();
+
+    // print the address of the deployed contract
+  console.log(
+    "Verify Contract Address",
+    deployedRandomWinnerGameContract.address
+  );
+
+  console.log("Sleeping....");
+  //wait for the etherscan to notice that the contract has been deployed
+  await sleep(30000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify",{
+    address: deployedRandomWinnerGameContract.address,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  })
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms) {
+  return new Promise((resolve)=> setTimeout(resolve,ms))
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
